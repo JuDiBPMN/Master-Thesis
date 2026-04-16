e3a594552e1e83f530dffee6a6f14d513e839b57
JuDiBPMN <thesisbpmn@gmail.com>
2026-04-16 14:05:05 +0200

    XML cases voor evaluatie

--- xml_generation/pipeline.py
+++ xml_generation/pipeline.py
@@ -732,9 +732,9 @@ def generate_case_bpmn_xml(
 
 if __name__ == "__main__":
     # ── CONFIGURATION ─────────────────────────────────────────────────────────
-    case_name          = "case_23"
+    case_name          = "case_22"
     pipeline_name      = "direct_extraction_pipeline"
-    prompting_strategy = "mistral_zero_shot"
+    prompting_strategy = "phi4_A_few_shot"
     # ──────────────────────────────────────────────────────────────────────────
 
     SCRIPT_DIR   = os.path.dirname(os.path.abspath(__file__))
