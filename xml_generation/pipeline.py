50becada40c47eecb55e9ca372766d3c92214395
JuDiBPMN <thesisbpmn@gmail.com>
2026-04-21 13:50:34 +0200

    xml

--- xml_generation/pipeline.py
+++ xml_generation/pipeline.py
@@ -732,9 +732,9 @@ def generate_case_bpmn_xml(
 
 if __name__ == "__main__":
     # ── CONFIGURATION ─────────────────────────────────────────────────────────
-    case_name          = "case_21"
+    case_name          = "case_28"
     pipeline_name      = "direct_extraction_pipeline"
-    prompting_strategy = "mistral_finetuned"
+    prompting_strategy = "fine_tuned_phi4"
     # ──────────────────────────────────────────────────────────────────────────
 
     SCRIPT_DIR   = os.path.dirname(os.path.abspath(__file__))
